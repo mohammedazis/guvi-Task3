0f43c14eb273d0519bae43b32dc097744ea91bb9
naanmudhalvan-SI <124564500+mohammedazis@users.noreply.github.com>
2023-05-26 19:10:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//       1)commparing the two json objects with their properties
+var data1 = {name: "John",age: 30,city: "New York"}
+ var data2 =  {name: "Jane",age: 25,city: "Los Angeles"}
+  
+console.log(JSON.stringify(data1)==JSON.stringify(data2));
